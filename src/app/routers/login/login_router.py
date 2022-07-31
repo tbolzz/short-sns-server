161e0f6a718306a531c071b63f2a201ec54b8f20
cshoon1226 <cshoon951226@gmail.com>
2022-08-01 01:54:28 +0900

    login - 조회 Test 수정

--- src/app/routers/login/login_router.py
+++ src/app/routers/login/login_router.py
@@ -7,6 +7,8 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from src.app.models.schema.login_schemas import fake_users_db, fake_users_db2, User, UserInDB, Token, TokenData
+from src.app.models.schema.story_schemas import StoryResponse
+from src.app.models.type.story_types import PublishScopeType, StoryStatusType
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -105,7 +107,10 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
 
-@login_router.get("/items",
-                  description="Token 조회 Test")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+@login_router.get("/items/{diary_story_id}",
+                  description="조회 Test")
+async def read_items(diary_story_id: int,
+                     token: str = Depends(oauth2_scheme),
+                     ):
+    current_user = await get_current_user(token)
+    return {"current_user": current_user, "diary_story_id": diary_story_id}
